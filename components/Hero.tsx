75cae183a796e4ee2333933bfd4d56eac473b361
thecodingrunner <finn.moffett@gmail.com>
2024-06-26 21:59:27 +0100

    Escape hyphen with &apos;

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='h-[60vh] bg-primary-color flex justify-center items-center flex-col'>
         {/* <img src={bg.src} alt='background' className='w-3/4' /> */}
         {/* <h1 className='text-[10rem]'>mr bates revision</h1> */}
-        <p className='text-center w-3/4 text-5xl leading-relaxed -mt-6'>Whether you're grappling with challenging concepts or looking to refine your exam strategies, mr bates revision is your go-to resource for academic success. Join me, and let's make learning an enjoyable and rewarding experience!</p>
+        <p className='text-center w-3/4 text-5xl leading-relaxed -mt-6'>Whether you&apos;re grappling with challenging concepts or looking to refine your exam strategies, mr bates revision is your go-to resource for academic success. Join me, and let&apos;s make learning an enjoyable and rewarding experience!</p>
     </div>
   )
 }
